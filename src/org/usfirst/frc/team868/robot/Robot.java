772e085f1e40e7903f6c9916f46888afbb755341
Caleb Smith <karagenit@outlook.com>
2018-02-11 15:19:44 -0500

    Update Main Robot File

--- src/org/usfirst/frc/team868/robot/Robot.java
+++ src/org/usfirst/frc/team868/robot/Robot.java
@@ -20,25 +20,14 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
 public class Robot extends TimedRobot {
 	
 	public static final Collector collector = new Collector();
 	public static final Arm arm = new Arm();
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
-		SmartDashboard.putData("Toggle Solenoid", new ToggleArm());
+		SmartDashboard.putData("Toggle Arm Position", new ToggleArm());
 		SmartDashboard.putData("Simple Power", new SimpleCollector());
 		SmartDashboard.putData("Close via Rangefinder", new RangefinderTriggerArm());
 		SmartDashboard.putData("Oscillating Power", new OscillateCollector());
@@ -46,60 +35,8 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putData("Limit Current Draw", new LimitPowerCollector());
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
-
-	}
-
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-	}
-
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	public void teleopInit() {
-	}
-
-	/**
-	 * This function is called periodically during operator control.
-	 */
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
-	}
-
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
 	}
 }

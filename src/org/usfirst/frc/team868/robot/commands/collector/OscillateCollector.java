92ae0bd41e5521af4e5bd591951c9904230123cd
Caleb Smith <karagenit@outlook.com>
2018-02-11 15:05:18 -0500

    Implement Oscillating Power

--- src/org/usfirst/frc/team868/robot/commands/collector/OscillateCollector.java
+++ src/org/usfirst/frc/team868/robot/commands/collector/OscillateCollector.java
@@ -1,36 +1,66 @@
 package org.usfirst.frc.team868.robot.commands.collector;
 
+import org.usfirst.frc.team868.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class OscillateCollector extends Command {
 
-    public OscillateCollector() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	private Timer timer = new Timer();
+	private boolean rightPowered = false;
+	@SuppressWarnings("unused") //technically never used, but helpful
+	private boolean leftPowered = false;
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public OscillateCollector() {
+		requires(Robot.collector);
+		SmartDashboard.putNumber("Collector Power", 1);
+		SmartDashboard.putNumber("Oscillation Time", 100); //ms, time high (aka 1/2 period)
+		SmartDashboard.putBoolean("Oscillation Offset", false); //oscillate out-of-phase
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.collector.configDefaults();
+		timer.start();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double power = SmartDashboard.getNumber("Collector Power", 1);
+		double time = SmartDashboard.getNumber("Oscillation Time", 100);
+		boolean offset = SmartDashboard.getBoolean("Oscillation Offset", false);
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+		if (timer.hasPeriodPassed(time/1000)) {
+			if (offset) {
+				if (rightPowered) {
+					rightPowered = false;
+					leftPowered = true;
+					Robot.collector.setMotors(0, power);
+				} else { //either left is powered or neither are
+					rightPowered = true;
+					leftPowered = false;
+					Robot.collector.setMotors(power, 0);
+				}
+			} else {
+				if (rightPowered) {
+					rightPowered = false;
+					leftPowered = false;
+					Robot.collector.setMotors(0, 0);
+				} else {
+					rightPowered = true;
+					leftPowered = true;
+					Robot.collector.setMotors(power, power);
+				}
+			}
+		}
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	protected boolean isFinished() {
+		return false;
+	}
 }

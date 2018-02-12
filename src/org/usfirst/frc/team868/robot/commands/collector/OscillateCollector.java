e2e2a2d372f194c123308d081ac91ca71073fdb0
Caleb Smith <karagenit@outlook.com>
2018-02-11 20:41:04 -0500

    Use Sin/Cos in Oscillation Command

--- src/org/usfirst/frc/team868/robot/commands/collector/OscillateCollector.java
+++ src/org/usfirst/frc/team868/robot/commands/collector/OscillateCollector.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team868.robot.commands.collector;
 
 import org.usfirst.frc.team868.robot.Robot;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -11,51 +10,37 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class OscillateCollector extends Command {
 
-	private Timer timer = new Timer();
-	private boolean rightPowered = false;
-	@SuppressWarnings("unused") //technically never used, but helpful
-	private boolean leftPowered = false;
-
 	public OscillateCollector() {
 		requires(Robot.collector);
 		SmartDashboard.putNumber("Collector Power", 1);
-		SmartDashboard.putNumber("Oscillation Time", 100); //ms, time high (aka 1/2 period)
+		SmartDashboard.putNumber("Oscillation Time", 0.5); //s, time high (aka 1/2 period)
 		SmartDashboard.putBoolean("Oscillation Offset", false); //oscillate out-of-phase
 	}
 
 	protected void initialize() {
 		Robot.collector.configDefaults();
-		timer.start();
 	}
 
 	protected void execute() {
 		double power = SmartDashboard.getNumber("Collector Power", 1);
-		double time = SmartDashboard.getNumber("Oscillation Time", 100);
-		boolean offset = SmartDashboard.getBoolean("Oscillation Offset", false);
+		double time = SmartDashboard.getNumber("Oscillation Time", 0.5);
+		boolean outOfPhase = SmartDashboard.getBoolean("Oscillation Offset", false);
 
-		if (timer.hasPeriodPassed(time/1000)) {
-			if (offset) {
-				if (rightPowered) {
-					rightPowered = false;
-					leftPowered = true;
-					Robot.collector.setMotors(0, power);
-				} else { //either left is powered or neither are
-					rightPowered = true;
-					leftPowered = false;
-					Robot.collector.setMotors(power, 0);
-				}
-			} else {
-				if (rightPowered) {
-					rightPowered = false;
-					leftPowered = false;
-					Robot.collector.setMotors(0, 0);
-				} else {
-					rightPowered = true;
-					leftPowered = true;
-					Robot.collector.setMotors(power, power);
-				}
-			}
+		double powerBase = .75 * power;					// Average will be 75% of max power setting
+		double powerFluctuate = .25 * power;			// How much to fluctuation by (plus gets us to 100% power, minus gets to 50% power)
+		double secsRunning = timeSinceInitialized();	// How long the command has been running
+		double timeScaler = time / (2.0 * Math.PI);		// Controls how long it takes for a full oscillation (1/2 sec for full oscillation)
+
+		double powerOutLeft = powerBase + powerFluctuate * Math.sin(secsRunning * timeScaler);
+		double powerOutRight;
+
+		if (outOfPhase) {
+			powerOutRight = powerBase + powerFluctuate * Math.cos(secsRunning * timeScaler);
+		} else {
+			powerOutRight = powerOutLeft;
 		}
+
+		Robot.collector.setMotors(powerOutLeft, powerOutRight);
 	}
 
 	protected boolean isFinished() {
